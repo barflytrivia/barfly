2043195175d8657a0ad22f44b0ae21c6666f8320
doug97703 <douglasjtaylor94@gmail.com>
2020-03-12 10:33:19 -0700

    working backend with API routes

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const server = require('./lib/server.js');
+const socket = require('./lib/trivia-server');
+
 require('dotenv').config();
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const mongooseOptions = {
-  useNewUrlParser:true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-};
+// const mongooseOptions = {
+//   useNewUrlParser:true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// };
 
-mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
-
-server.start(process.env.PORT);
+// mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
+server.start(3000);
